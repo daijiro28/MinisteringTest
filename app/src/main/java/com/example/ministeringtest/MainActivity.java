c9d83e1d5ea397e86bf72d54faccf58debafdd84
Daijiro Sagane <daijiro.2812@i.softbank.jp>
2020-11-28 18:22:16 +0900

    Initial commit

---
+++ app/src/main/java/com/example/ministeringtest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.ministeringtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, FirebaseUIActivity.class);
+        startActivity(intent);
+    }
+}
